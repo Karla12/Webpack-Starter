0562b218883bcc0edf69c5863096ef75c6867b10
Karla Evelyn Espino Maldonado <karlaevelyn098@gmail.com>
2020-01-31 17:55:12 -0600

    174678: Create two promise for get pokemos 1 and 2 generations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,8 @@ console.log('webpack starterkit');
 
 const axios = require('axios');
 
+//[ARJ 1]
+
 var list_pokemons = document.getElementById('pokemons');
 
 const getPokemons = async () =>
@@ -24,3 +26,56 @@ const getPokemons = async () =>
       .catch(error => error);
 
 const pokemons = getPokemons();
+
+// [ARJ 2]
+
+var pokemons_arj_2_p1 = document.getElementById('pokemons_arj_2_p1');
+var pokemons_arj_2_p2 = document.getElementById('pokemons_arj_2_p2');
+
+const fetchPokemonsGenerationOne = () => {
+  const p1 = axios.get('https://pokeapi.co/api/v2/generation/1/')
+    .then(response => { 
+      const {
+        data: {
+          pokemon_species
+        }
+      } = response;
+      return pokemon_species;
+    })
+    .catch(error => { return error; });
+
+  return p1;
+}
+
+const fetchPokemonsGenerationTwo = () => {
+  const p2 = axios.get('https://pokeapi.co/api/v2/generation/2/')
+  .then(response => {
+    const {
+        data: {
+          pokemon_species
+        }
+      } = response;
+      return pokemon_species;
+  })
+  .catch(error => { return error; })
+  return p2;
+}
+
+Promise.all([fetchPokemonsGenerationOne(),fetchPokemonsGenerationTwo()])
+  .then(response => {
+    response.map((value, key) => {
+      let k = key;
+      value.map((value, key) => {
+            var content = key +' - ' + value['name'];
+            var li = document.createElement("li");
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(content));
+            if (k === 0) {
+              pokemons_arj_2_p1.appendChild(li).appendChild(p);
+            } else {
+              pokemons_arj_2_p2.appendChild(li).appendChild(p);
+            }
+          });
+    });
+  })
+  .catch(error => console.log(error));
